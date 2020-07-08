26aac0a274284b24ec92ecf03c57f49be7e05ec7
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2020-07-08 18:05:03 +0100

    Remove unused variable

--- assets/js/minimalexample/minimal.js
+++ assets/js/minimalexample/minimal.js
@@ -1,7 +1,5 @@
 // render essentials
 var scene, camera, renderer;
-var guiElements;
-
 
 // init scene, camera, model and renderer
 function init() {
@@ -9,33 +7,32 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 'skyblue' );
 
+    // CANVAS
     var canvasHolder = document.getElementById('canvas-holder');
     var width = canvasHolder.clientWidth;
     var height = width * 0.8;
     canvasHolder.clientHeight = height;
 
+    // CAMERA
     const fov = 35; // AKA Field of View
     const near = 0.1; // the near clipping plane
     const far = 100; // the far clipping plane
-
     const camera = new THREE.PerspectiveCamera( fov, width / height, near, far );
-
     camera.position.set( 0, 0, 10 );
 
+    // OBJECT
     const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
     const material = new THREE.MeshBasicMaterial();
     const mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    // renderer setup
+    // RENDERER
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     canvasHolder.appendChild( renderer.domElement );
     renderer.render( scene, camera );
 }
 
-
-// load dat menu
 window.onload = function() {
     // Hurray!
     init();
